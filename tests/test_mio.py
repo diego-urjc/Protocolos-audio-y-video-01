1d9d150958cae5a16afe1f0c3b617d67362c1d85
Diego Lucas Luis <d.lucas.2021@alumnos.urjc.es>
2024-10-01 22:31:46 +0200

    Ejercicio 4

---
+++ tests/test_mio.py
@@ -0,0 +1,16 @@
+# Ejercicio 4
+
+import unittest
+from mysound import Sound
+
+
+class Testvalues(unittest.TestCase):
+
+    def testValues(self):
+        sound = Sound(1)
+        sound.sin(440, 10000)
+
+        # comprobar que la duracion de la onda es mayor a 0 y que es un numero float o entero
+
+        self.assertTrue(sound.duration > 0)
+        self.assertTrue(type(sound.duration) == float or int)
